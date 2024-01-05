f57b1f9e690ea756771b5be93cc0d53d6636de18
SobczakL <lucassobczak3@gmail.com>
2024-01-04 20:41:33 -0500

    test with passed in string

--- hello_test.go
+++ hello_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func TestHello()  {
-    got := Hello()
-    want := "Hello, world"
+func TestHello(t *testing.T)  {
+    got := Hello("Chris")
+    want := "Hello, Chris"
 
     if got != want{
         t.Errorf("got %q want %q", got, want)
